5cbdf395054554e92fd21670fe681cc7c734f338
Gabriela Acha <gabriela.gabriela.acha.acha@gmail.com>
2020-06-29 09:03:31 +0200

    react

---
+++ 06-2020/24-06-country-hadis-solution/src/components/Loading.js
@@ -0,0 +1,13 @@
+import React from "react";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+function Spinner() {
+  return (
+    <div className="loading">
+      <p>Loading...</p>
+      {/* <FontAwesomeIcon icon="spinner" className="icon" spin /> */}
+    </div>
+  );
+}
+
+export default Spinner;
